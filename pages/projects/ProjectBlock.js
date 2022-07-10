781f8e61de57ea6fb504a2c67ecd54decb2d3785
David White <whited9339@gmail.com>
2022-07-10 12:08:59 -0600

    Styled project block

--- pages/projects/ProjectBlock.js
+++ pages/projects/ProjectBlock.js
@@ -1,13 +1,21 @@
 import { 
   projectBlock,
-  title
+  title,
+  tagline
 } from "../../styles/projectBlock.module.scss";
 import SlideShow from "../../components/SlideShow";
+import { useRouter } from "next/router";
 
 export default ({ project }) => {
+  const router = useRouter();
+
   return (
-    <div className={projectBlock}>
+    <div 
+      className={projectBlock}
+      onClick={() => router.push(`/projects/${project.title}`)}
+    >
       <p className={title}>{project.title}</p>
+      <p className={tagline}>{project.tagline}</p>
       <SlideShow slides={project.imageUrls} />
     </div>
   )
