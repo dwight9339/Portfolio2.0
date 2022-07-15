e6c71f9ccceee3019072fff77b06a0097d3c2a4a
David White <whited9339@gmail.com>
2022-07-15 15:59:42 -0600

    Implemented nodemailer email sending

--- utils/sendInquiryEmail.js
+++ utils/sendInquiryEmail.js
@@ -1,7 +1,31 @@
-const sendInquiryEmail = () => {
+import { createTransport } from "nodemailer";
 
+const sendInquiryEmail = (visitorMessage) => {
+  const {
+    INQUIRY_EMAIL_SERVICE,
+    INQUIRY_EMAIL_FROM,
+    INQUIRY_EMAIL_PW,
+    INQUIRY_EMAIL_TO
+  } = process.env;
+  const transporter = createTransport({
+    service: INQUIRY_EMAIL_SERVICE,
+    auth: {
+      user: INQUIRY_EMAIL_FROM,
+      pass: INQUIRY_EMAIL_PW
+    }
+  });
 
-
+  try {
+    const result = await transporter.sendMail({
+      from: INQUIRY_EMAIL_FROM,
+      to: INQUIRY_EMAIL_TO,
+      subject: "Portfolio Inquiry",
+      text: visitorMessage
+    });
+  } catch(err) {
+    console.error(err);
+  }
+  
 }
 
 export default sendInquiryEmail;
