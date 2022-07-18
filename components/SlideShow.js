22327a561f7b7cdd38e043493aa1618b040cca60
David White <whited9339@gmail.com>
2022-07-18 09:38:01 -0600

    Add p5 sketch component

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -1,13 +1,28 @@
+import dynamic from "next/dynamic";
+
+const Sketch = dynamic(async () => {
+  const mod = await import ("react-p5");
+  return mod.default;
+}, {
+  ssr: false
+});
+
 const SlideShow = ({ imageUrls }) => {
+  const setup = (p5, canvasParentRef) => {
+		p5.createCanvas(200, 200).parent(canvasParentRef);
+	};
+
+  const draw = (p5) => {
+    p5.background(0);
+    p5.square(100, 100, 50);
+  };
+
   return (
-    <div
-      style={{
-        backgroundImage: `url(${imageUrls[0]})`,
-        width: "600px",
-        height: "338px"
-      }}
-    ></div>
-  )
+    <Sketch
+      setup={setup}
+      draw={draw}
+    />
+  );
 }
 
 export default SlideShow;
