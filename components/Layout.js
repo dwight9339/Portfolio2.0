69f7f4aa48e20175ad84bb661e0bff494d9698f1
David White <whited9339@gmail.com>
2022-07-19 10:25:04 -0600

    Positioned threejs canvas in background

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,45 @@
 import Header from "./Header";
-import { Canvas, useFrame } from "@react-three/fiber";
-import useThreejsBackground from "hooks/useThreejsBackground";
 import { 
   layout, 
-  page
+  page,
+  threeCanvas
 } from "../styles/layout.module.scss";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Cone = () => {
+  const mesh = useRef();
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += 0.001;
+    mesh.current.rotation.z += 0.003;
+  });
+
+  return (
+    <mesh
+      ref={mesh}
+    >
+      <coneGeometry args={[2, 3, 6]} />
+      <meshStandardMaterial
+        wireframe
+        color="#aef900"
+      />
+    </mesh>
+  );
+}
 
 const Layout = ({ children }) => {
-  useThreejsBackground();
-
   return (
     <div className={layout}>
       <Header />
       <div className={page}>
         {children}
+      </div>
+      <div className={threeCanvas}>
+        <Canvas>
+          <ambientLight />
+          <Cone />
+        </Canvas>
       </div>
     </div>
   );
