63c8f2e0caf00fc466e2a40a9e88bcde629ea07d
David White <whited9339@gmail.com>
2022-07-19 17:32:12 -0600

    Check width parameter before rendering

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { WaterfallSlideshow } from "utils/p5Slideshow";
+import { useRouter } from "next/router";
 
 const Sketch = dynamic(async () => {
   const mod = await import ("react-p5");
@@ -10,46 +11,55 @@ const Sketch = dynamic(async () => {
 });
 
 const SlideShow = ({ imageUrls, width }) => {
-  const myWidth = width * 0.95;
+  const router = useRouter();
   const aspectRatio = 9/16;
-  const [images, setImages] = useState([]);
-  let waterfall;
+  const myWidth = width * 0.9;
 
-  useEffect(() => {
-    console.log(`images: ${images}`);
-  }, [images]);
+  // const [images, setImages] = useState([]);
+  // let waterfall;
 
-  const preload = (p5) => {
-    let imgs = [];
+  
 
-    imageUrls.forEach((url) => {
-      p5.loadImage(url, (img) => {
-        img.resize(myWidth, 0);
-        imgs.push(img);
+  const sketch = useMemo(() => {
+    if (!myWidth) return null;
+
+    let images = [];
+    let waterfall;
+
+    const preload = (p5) => {
+      imageUrls.forEach((url) => {
+        p5.loadImage(url, (img) => {
+          img.resize(myWidth, 0);
+          images.push(img);
+        });
       });
-    });
-
-    setImages(() => imgs);
-  }
-
-  const setup = (p5, canvasParentRef) => {
-		p5
-      .createCanvas(myWidth, myWidth * aspectRatio)
-      .parent(canvasParentRef); 
-
+    }
+  
+    const setup = (p5, canvasParentRef) => {
+      p5
+        .createCanvas(myWidth, myWidth * aspectRatio)
+        .parent(canvasParentRef); 
+  
       waterfall = new WaterfallSlideshow(images, p5.height, 0.5);
-  	};
+    };
+  
+    const draw = (p5) => {
+      waterfall.draw(p5);
+    };
 
-  const draw = (p5) => {
-    waterfall.draw(p5);
-  };
+    return (
+      <Sketch
+        preload={preload}
+        setup={setup}
+        draw={draw}
+      />
+    );
+  }, [myWidth]);
 
   return (
-    <Sketch
-      preload={preload}
-      setup={setup}
-      draw={draw}
-    />
+    <>
+      {sketch}
+    </>
   );
 }
 
