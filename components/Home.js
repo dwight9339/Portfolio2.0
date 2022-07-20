ee6739bc0ef7709aea71ae4372a811b1eeda5bb8
David White <whited9339@gmail.com>
2022-07-20 14:21:38 -0600

    Filled out home page

--- components/Home.js
+++ components/Home.js
@@ -1,29 +1,63 @@
 import {
   container,
-  introCard
+  introCard,
+  projectIcon,
+  galleryIcon,
+  aboutIcon,
+  contactIcon
 } from "/styles/home.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <div className={container}>
       <div className={introCard}>
         <h2>Welcome to my portfolio</h2>
+        <div className={projectIcon}>
+          <Image
+            src="/images/project_icon.svg"
+            width={150}
+            height={150}
+          />
+        </div>
         <p>
-          If you would like to see some projects of mine you can 
-          find them <Link href="/projects"><a>here.</a></Link>
+          Check out the <Link href="/projects"><a>projects</a></Link> page 
+          to see some things that I've worked on.
         </p>
+        <div className={galleryIcon}>
+          <Image
+            src="/images/gallery_icon.png"
+            width={150}
+            height={150}
+          />
+        </div>
         <p>
           Head over to the <Link href="/gallery"><a>gallery</a></Link> to see 
           some cool code animations.
         </p>
+        <div className={aboutIcon}>
+          <Image
+            src="/images/about_icon.png"
+            width={150}
+            height={150}
+          />
+        </div>
         <p>
-          Check out the <Link href="/about"><a>about</a></Link> page to
-          learn a little more about me.
+          You can learn a little more about me and find links to my socials 
+          over on the <Link href="/about"><a>about</a></Link> page.
         </p>
+        <div className={contactIcon}>
+          <Image
+            src="/images/contact_icon.png"
+            width={150}
+            height={150}
+          />
+        </div>
         <p>
-          To drop me a line, you can use the conact 
-          form <Link href="/about"><a>here.</a></Link>
+          To drop me a line, you can use the form on 
+          the <Link href="/about"><a>contact</a></Link> page 
+          or email me at david@david-white.dev.
         </p>
       </div>
     </div>
