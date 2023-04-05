0ee2c8ad42d07cec9e688ef46a8e5f2b657ecedb
David White <whited9339@gmail.com>
2023-04-05 08:54:36 -0600

    Fixed bug where header disappears on scroll and doesn't return if user navigate to other page

--- components/Header.js
+++ components/Header.js
@@ -9,23 +9,31 @@ import {
   navItem
 } from "../styles/header.module.scss";
 import useScrolling from "../hooks/useScrolling";
+import { useRouter } from "next/router";
 import { useState, useEffect, useMemo } from "react";
 
 const Header = () => {
+  const router = useRouter();
   const { scrollingUp, yOffset } = useScrolling();
   const [headerShowing, setHeaderShowing] = useState(true);
   const [headerClass, setHeaderClass] = useState(header);
   const inStickyZone = useMemo(() => yOffset > 10, [yOffset]);
   
   useEffect(() => {
+    console.log("scrollingUp: ", scrollingUp);
+    console.log("headerShowing: ", headerShowing);
+    console.log("inStickyZone: ", inStickyZone);
     if (inStickyZone && headerShowing && !scrollingUp) {
       setHeaderShowing(false);
       setHeaderClass(headerSlideOut);
     } else if (inStickyZone && !headerShowing && scrollingUp) {
       setHeaderShowing(true);
       setHeaderClass(headerSlideIn);
-    } 
-  }, [scrollingUp, headerShowing, inStickyZone]);
+    } else if (!inStickyZone && !headerShowing) {
+      setHeaderShowing(true);
+      setHeaderClass(headerSlideIn);
+    }
+  }, [router.asPath, scrollingUp, headerShowing, inStickyZone]);
 
   return (
     <header className={headerClass}>
