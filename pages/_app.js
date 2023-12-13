c8093e57c22540e937cb6e916c0c7a27f74b9476
David White <whited9339@gmail.com>
2023-12-13 12:14:47 -0700

    Add Vercel analytics package

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "styles/global.scss";
 import Layout from "components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />  
+      <Analytics />
     </Layout>
   );
 }
