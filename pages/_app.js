45c602318bf708bf69e115bb65e8fe403c59c05f
David White <david@david-white.dev>
2024-03-14 11:38:14 -0600

    Added Vercel speed insights component to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "styles/global.scss";
 import Layout from "components/Layout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />  
       <Analytics />
+      <SpeedInsights />
     </Layout>
   );
 }
