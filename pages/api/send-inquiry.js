286ca7b03bebf68404c02088326009c88295dde8
David White <david@david-white.dev>
2024-03-16 13:04:05 -0600

    Added validation to send-inquiry endpoint

--- pages/api/send-inquiry.js
+++ pages/api/send-inquiry.js
@@ -1,12 +1,31 @@
 import sendInquiryEmail from "utils/sendInquiryEmail";
 
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+    this.status = 400;
+  }
+}
+
+const checkEmailFormat = (str) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(str).toLowerCase());
+};
+
 const sendInquiry = async (req, res) => {
   try {
+    const { name, email, message } = req.body;
+    if (!name) throw new ValidationError("Please include a name");
+    if (!email) throw new ValidationError("Please include an email");
+    if (!checkEmailFormat(email)) throw new ValidationError("Invalid email");
+    if (!message) throw new ValidationError("Please include a message");
+
     await sendInquiryEmail(req.body);
     res.status(200).send();
   } catch(err) {
-    console.error(err);
-    res.status(500).send();
+    const status = err.name && err.name === "ValidationError" ? err.status : 500;
+    res.status(status).json({ msg: `${err}`});
   }
 }
 
